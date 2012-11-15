91591776f2868c9d6681c7f08ad9ed2ba42fd75b
danila <kdanilke@gmail.com>
2012-11-15 06:19:23 +0100

    ddd1

--- lib/redgem/engine.rb
+++ lib/redgem/engine.rb
@@ -5,7 +5,7 @@ module Redgem
   class Engine < Rails::Engine
      isolate_namespace Redgem
 
-	initializer 'redgem' do |app|
+	  initializer 'redgem' do |app|
 
 		ActiveSupport.on_load :after_initialize do
 		    app.routes.prepend do
@@ -15,24 +15,7 @@ module Redgem
 			 match 'timesheet/reset', :controller => 'timesheet', :action => 'reset', :conditions => { :method => :delete }
 		    end
 		end
-		
-        pattern = pattern_from app.config.i18n.available_locales
-        add("rails/locale/#{pattern}.yml")
-    
-
-    end
-   
-
-    protected
-
-    def self.add(pattern)
-      files = Dir[File.join(File.dirname(__FILE__), '../..', pattern)]
-      I18n.load_path.concat(files)
-    end
-
-    def self.pattern_from(args)
-      array = Array(args || [])
-      array.blank? ? '*' : "{#{array.join ','}}"
+		Dir.glob( File.dirname(__FILE__) + "lib/locales/*.{rb,yml}" ) 
     end
 
 
