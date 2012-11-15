d206dd0e2b79a0b723176f262256751ef3e38322
danila <kdanilke@gmail.com>
2012-11-15 03:31:37 +0100

    cpp,

--- lib/redgem/engine.rb
+++ lib/redgem/engine.rb
@@ -15,7 +15,25 @@ module Redgem
 			 match 'timesheet/reset', :controller => 'timesheet', :action => 'reset', :conditions => { :method => :delete }
 		    end
 		end
+		I18nCountryTranslations::Railtie.instance_eval do
+        pattern = pattern_from app.config.i18n.available_locales
 
+        add("rails/locale/#{pattern}.yml")
+      end
+
+    end
+   
+
+    protected
+
+    def self.add(pattern)
+      files = Dir[File.join(File.dirname(__FILE__), '../..', pattern)]
+      I18n.load_path.concat(files)
+    end
+
+    def self.pattern_from(args)
+      array = Array(args || [])
+      array.blank? ? '*' : "{#{array.join ','}}"
     end
 
 
